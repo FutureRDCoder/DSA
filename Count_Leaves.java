87499753b3c65f0b13951c58ddc9817bb88a422c
Sundharam2.0 <101635539+FutureRDCoder@users.noreply.github.com>
2025-04-23 10:04:40 +0530

    Create Count_Leaves.java

---
+++ Count_Leaves.java
@@ -0,0 +1,9 @@
+int countLeaves(Node root) {
+    if (root == null) {
+        return 0;
+    }
+    if (root.left == null && root.right == null) {
+        return 1;
+    }
+    return countLeaves(root.left) + countLeaves(root.right);
+}
