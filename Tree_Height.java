e1a452bbe4a5c0b35a46622f0dd2fae8195d1ee7
Sundharam2.0 <101635539+FutureRDCoder@users.noreply.github.com>
2025-04-23 10:04:11 +0530

    Create Tree_Height.java

---
+++ Tree_Height.java
@@ -0,0 +1,13 @@
+height
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(maxDepth(root.left),maxDepth(root.right) ) + 1;
+    }
+}
